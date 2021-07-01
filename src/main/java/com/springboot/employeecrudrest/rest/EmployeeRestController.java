c03ca9c05d214fa321a12ad0bc40854a4b6df465
Ghazal <ghazal.kazemi@gmail.com>
2021-07-01 14:13:10 +1000

    Implement get mapping for finding employee by id

--- src/main/java/com/springboot/employeecrudrest/rest/EmployeeRestController.java
+++ src/main/java/com/springboot/employeecrudrest/rest/EmployeeRestController.java
@@ -5,6 +5,7 @@ import com.springboot.employeecrudrest.entity.Employee;
 import com.springboot.employeecrudrest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,15 @@ public class EmployeeRestController {
     public List<Employee> findAll(){
         return employeeService.findAll();
     }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee findById(@PathVariable int employeeId){
+        Employee employee = employeeService.findById(employeeId);
+        if (employee == null){
+            throw new RuntimeException("Employee not found - " + employeeId);
+        }
+        return employee;
+    }
+
 
 }
