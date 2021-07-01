5d0d4ed45c217aa334ba862fcc94acb2bea1bf63
Ghazal <ghazal.kazemi@gmail.com>
2021-07-01 14:03:46 +1000

    Inject employeeDAO

--- src/main/java/com/springboot/employeecrudrest/service/EmployeeServiceImpl.java
+++ src/main/java/com/springboot/employeecrudrest/service/EmployeeServiceImpl.java
@@ -1,13 +1,19 @@
 package com.springboot.employeecrudrest.service;
 
+import com.springboot.employeecrudrest.dao.EmployeeDAO;
 import com.springboot.employeecrudrest.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
 
-
+    private EmployeeDAO employeeDAO;
+    @Autowired
+    public EmployeeServiceImpl(EmployeeDAO employeeDAO){
+        this.employeeDAO = employeeDAO;
+    }
 
     @Override
     public List<Employee> findAll() {
