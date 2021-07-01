ba7117a7d8d3721d07b00c993a9b665b699f09c2
Ghazal <ghazal.kazemi@gmail.com>
2021-07-01 14:05:16 +1000

    Delegate the calls to emloyeeDAO

--- src/main/java/com/springboot/employeecrudrest/service/EmployeeServiceImpl.java
+++ src/main/java/com/springboot/employeecrudrest/service/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import com.springboot.employeecrudrest.dao.EmployeeDAO;
 import com.springboot.employeecrudrest.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 @Service
@@ -16,22 +17,26 @@ public class EmployeeServiceImpl implements EmployeeService{
     }
 
     @Override
+    @Transactional
     public List<Employee> findAll() {
-        return null;
+        return employeeDAO.findAll();
     }
 
     @Override
+    @Transactional
     public Employee findById(int employeeId) {
-        return null;
+        return employeeDAO.findById(employeeId);
     }
 
     @Override
+    @Transactional
     public void save(Employee employee) {
-
+        employeeDAO.save(employee);
     }
 
     @Override
+    @Transactional
     public void deleteById(int employeeId) {
-
+        employeeDAO.deleteById(employeeId);
     }
 }
